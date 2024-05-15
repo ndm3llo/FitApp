23fab9b2d7b6a37753803153f22066e1f62e84be
ndm3llo <ndmello@umich.edu>
2024-05-15 10:41:00 -0400

    Adding beginning files

---
+++ FitApp/FitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitAppApp.swift
+//  FitApp
+//
+//  Created by Nicolas D'Mello on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
